08e44b16b4ce049b753b0702276737c014ff9fd7
Ilia Trubitsyn <ilia_trubitsyn@epam.com>
2018-09-09 20:40:14 +0300

    add pointcuts and advices

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,6 +4,9 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -20,6 +23,24 @@ public class LoggingAspect {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
+    public void afterRepoFind(JoinPoint joinPoint){
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is finished");
+    }
+
+    @Around("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
+    public long timeCount(ProceedingJoinPoint joinPoint) throws Throwable {
+        long begin=System.currentTimeMillis();
+
+        joinPoint.proceed();
+
+        long end=System.currentTimeMillis();
+
+        return end-begin;
     }
 
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -7,4 +7,16 @@ public class PointcutContainer {
     @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
     public void repositoryFind() {
     }
+
+    @Pointcut("within(com.andreitop.newco.controller.*)")
+    public void controllerFind(){
+    }
+
+    @Pointcut("this(com.andreitop.newco.repository..TripRepository)")
+    public void thisFind(){
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.controller.*Controller.find*())")
+    public void controllerFindAll(){
+    }
 }
